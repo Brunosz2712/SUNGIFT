081b62b47a5a0796bad4167cd58b92fc93999a43
ludanii <ludanielle2005@hotmail.com>
2024-11-19 04:10:15 -0300

    Adicionando aviso para voltar ao login

--- gs-lal/src/app/doacao/page.tsx
+++ gs-lal/src/app/doacao/page.tsx
@@ -5,6 +5,7 @@ import { useAtom } from 'jotai';
 import { useRouter } from 'next/navigation';
 import { userAtom } from '@/atoms';
 import type { Doacao } from '@/types';
+import { WarningOutlined } from '@ant-design/icons';
 
 export default function Doacao() {
     const [user] = useAtom(userAtom)
@@ -18,197 +19,214 @@ export default function Doacao() {
     const [errors, setErrors] = useState<{ [key: string]: string }>({})
     const [successMessage, setSuccessMessage] = useState("");
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const { name, value } = e.target;
-        setDoacao((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    };
 
-    const chamadaApi = async () => {
-        const response = await fetch(`http://localhost:8080/doacao/${user?.ID_CONDOMINIO}`)
+    if (!user) {
+      const volta = setTimeout(() => {
+        router.push("/login")
+      }, 3000)
+      return (
+        <>
+          <div className="min-h-screen flex justify-center items-center">
+            <h1 className="p-8 bg-[#97987eae] text-white rounded-lg shadow-md text-center h-full text-3xl">
+              <WarningOutlined /> é necessario o login primeiro! <br />
+              Redirecionando para a página de login...
+            </h1>
+          </div>
+          {volta}
+        </>
+      )
+    } else {
+      const handleChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+      ) => {
+        const { name, value } = e.target
+        setDoacao((prev) => ({
+          ...prev,
+          [name]: value,
+        }))
+      }
+
+      const chamadaApi = async () => {
+        const response = await fetch(
+          `http://localhost:8080/doacao/${user?.ID_CONDOMINIO}`
+        )
         const data = await response.json()
         setDoacaoGet(data)
-    }
+      }
 
-    useEffect(() => {
+      useEffect(() => {
         chamadaApi()
-    }, [])
+      }, [])
 
-    const validateForm = () => {
-        let newErrors: { [key: string]: string } = {};
-        if (!doacao.ID_MATERIAIS) newErrors.ID_MATERIAIS = "O tipo de material é obrigatório.";
-        if (!doacao.NR_QUANTIDADE) newErrors.NR_QUANTIDADE = "A quantidade é obrigatória.";
-        return newErrors;
-    };
+      const validateForm = () => {
+        let newErrors: { [key: string]: string } = {}
+        if (!doacao.ID_MATERIAIS)
+          newErrors.ID_MATERIAIS = "O tipo de material é obrigatório."
+        if (!doacao.NR_QUANTIDADE)
+          newErrors.NR_QUANTIDADE = "A quantidade é obrigatória."
+        return newErrors
+      }
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const validationErrors = validateForm();
+      const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const validationErrors = validateForm()
         if (Object.keys(validationErrors).length > 0) {
-            setErrors(validationErrors);
-            return;
+          setErrors(validationErrors)
+          return
         }
 
         try {
-            const response = await fetch("http://localhost:8080/doacao", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(doacao),
-            });
+          const response = await fetch("http://localhost:8080/doacao", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(doacao),
+          })
 
-            if (!response.ok) {
-                throw new Error("Erro ao cadastrar doacao. Tente novamente.");
-            }
+          if (!response.ok) {
+            throw new Error("Erro ao cadastrar doacao. Tente novamente.")
+          }
 
-            setSuccessMessage(
-              "Doacao cadastrada com sucesso!"
-            )
-
+          setSuccessMessage("Doacao cadastrada com sucesso!")
         } catch (error) {
-            console.error(error);
+          console.error(error)
         }
-    };
+      }
 
-    if (!user) {
-        router.push("/login")
-    }
+      return (
+        <main className="flex items-center justify-center min-h-screen bg-[#f4f4f4] py-8">
+          <div className="text-center w-full max-w-4xl items-center flex flex-col">
+            <h1 className="text-5xl font-bold text-[#97987E] mb-8">DOAÇÃO</h1>
+            {successMessage && (
+              <div className="w-full max-w-[900px] p-4 mb-6 bg-[#48d9717a] text-[#4a4a4a] rounded-lg shadow-md text-center">
+                <p className="font-semibold text-lg">{successMessage}</p>
+              </div>
+            )}
+            <form onSubmit={handleSubmit}>
+              <div className="mb-8">
+                <h2 className="text-2xl font-semibold text-[#505050] mb-4">
+                  MATERIAL
+                </h2>
+                <select
+                  value={doacao.ID_MATERIAIS}
+                  name="ID_MATERIAIS"
+                  onChange={handleChange}
+                  className="w-full max-w-[469.5px] h-[30.5px] bg-[#97987E66] text-[#505050] rounded-[15px] px-4 outline-none"
+                >
+                  <option value="">Selecione um material</option>
+                  <option value={2}>Alumínio</option>
+                  <option value={3}>Vidro</option>
+                  <option value={1}>Plástico</option>
+                  <option value={4}>Cobre</option>
+                  <option value={5}>Dispositivos Eletrônicos</option>
+                </select>
+              </div>
 
-    return (
-      <main className="flex items-center justify-center min-h-screen bg-[#f4f4f4] py-8">
-        <div className="text-center w-full max-w-4xl items-center flex flex-col">
-          <h1 className="text-5xl font-bold text-[#97987E] mb-8">DOAÇÃO</h1>
-          {successMessage && (
-            <div className="w-full max-w-[900px] p-4 mb-6 bg-[#48d9717a] text-[#4a4a4a] rounded-lg shadow-md text-center">
-              <p className="font-semibold text-lg">{successMessage}</p>
-            </div>
-          )}
-          <form onSubmit={handleSubmit}>
-            <div className="mb-8">
+              <div className="mb-8">
+                <h2 className="text-2xl font-semibold text-[#505050] mb-4">
+                  QUANTIDADE
+                </h2>
+                <input
+                  type="number"
+                  name="NR_QUANTIDADE"
+                  min="1"
+                  max="100"
+                  value={doacao.NR_QUANTIDADE}
+                  onChange={handleChange}
+                  placeholder="Digite a quantidade em KG (1 a 100)"
+                  className="w-full max-w-[469.5px] h-[30.5px] bg-[#97987E66] text-[#505050] rounded-[15px] px-4 outline-none mb-4"
+                />
+                <p className="text-sm text-[#707070] leading-relaxed text-justify w-full max-w-[469.5px]">
+                  Até <strong>10 kg</strong> ganhe <strong>5%</strong> de
+                  desconto, de <strong>11 a 20 kg</strong> ganhe{" "}
+                  <strong>10%</strong>, de <strong>21 a 30 kg</strong> ganhe{" "}
+                  <strong>15%</strong> e acima de <strong>30 kg</strong> ganhe{" "}
+                  <strong>20%</strong>. O valor máximo de desconto é{" "}
+                  <strong>20%</strong>!
+                </p>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="w-full max-w-[193px] h-[35px] bg-[#97987E] text-[#FFFFFF] text-xs font-bold uppercase rounded-[15px] hover:bg-[#6f7060] transition duration-200"
+                >
+                  Cadastrar
+                </button>
+              </div>
+            </form>
+            <div className="mt-8">
               <h2 className="text-2xl font-semibold text-[#505050] mb-4">
-                MATERIAL
+                Quantidade Total Acumulada:{" "}
+                {DoacaoGet.reduce((total, d) => total + d.NR_QUANTIDADE, 0)} kg
               </h2>
-              <select
-                value={doacao.ID_MATERIAIS}
-                name="ID_MATERIAIS"
-                onChange={handleChange}
-                className="w-full max-w-[469.5px] h-[30.5px] bg-[#97987E66] text-[#505050] rounded-[15px] px-4 outline-none"
-              >
-                <option value="">Selecione um material</option>
-                <option value={2}>Alumínio</option>
-                <option value={3}>Vidro</option>
-                <option value={1}>Plástico</option>
-                <option value={4}>Cobre</option>
-                <option value={5}>Dispositivos Eletrônicos</option>
-              </select>
             </div>
 
-            <div className="mb-8">
+            {/* Tabela de doações */}
+            <div className="mt-12 w-full">
               <h2 className="text-2xl font-semibold text-[#505050] mb-4">
-                QUANTIDADE
+                Doações Registradas
               </h2>
-              <input
-                type="number"
-                name="NR_QUANTIDADE"
-                min="1"
-                max="100"
-                value={doacao.NR_QUANTIDADE}
-                onChange={handleChange}
-                placeholder="Digite a quantidade em KG (1 a 100)"
-                className="w-full max-w-[469.5px] h-[30.5px] bg-[#97987E66] text-[#505050] rounded-[15px] px-4 outline-none mb-4"
-              />
-              <p className="text-sm text-[#707070] leading-relaxed text-justify w-full max-w-[469.5px]">
-                Até <strong>10 kg</strong> ganhe <strong>5%</strong> de
-                desconto, de <strong>11 a 20 kg</strong> ganhe{" "}
-                <strong>10%</strong>, de <strong>21 a 30 kg</strong> ganhe{" "}
-                <strong>15%</strong> e acima de <strong>30 kg</strong> ganhe{" "}
-                <strong>20%</strong>. O valor máximo de desconto é{" "}
-                <strong>20%</strong>!
-              </p>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="w-full max-w-[193px] h-[35px] bg-[#97987E] text-[#FFFFFF] text-xs font-bold uppercase rounded-[15px] hover:bg-[#6f7060] transition duration-200"
-              >
-                Cadastrar
-              </button>
-            </div>
-          </form>
-          <div className="mt-8">
-            <h2 className="text-2xl font-semibold text-[#505050] mb-4">
-              Quantidade Total Acumulada:{" "}
-              {DoacaoGet.reduce((total, d) => total + d.NR_QUANTIDADE, 0)} kg
-            </h2>
-          </div>
-
-          {/* Tabela de doações */}
-          <div className="mt-12 w-full">
-            <h2 className="text-2xl font-semibold text-[#505050] mb-4">
-              Doações Registradas
-            </h2>
-            <div className="overflow-x-auto">
-              <table className="w-full table-auto border-collapse border border-[#97987E66]">
-                <thead>
-                  <tr className="bg-[#f4f4f4]">
-                    <th className="px-4 py-2 text-[#505050] text-center whitespace-nowrap">
-                      Material
-                    </th>
-                    <th className="px-4 py-2 text-[#505050] text-center whitespace-nowrap">
-                      Quantidade (kg)
-                    </th>
-                    <th className="px-4 py-2 text-[#505050] text-center whitespace-nowrap">
-                      Descontos
-                    </th>
-                    <th className="px-4 py-2 text-[#505050] text-center whitespace-nowrap">
-                      Data
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {Array.isArray(DoacaoGet) && DoacaoGet.length > 0 ? (
-                    DoacaoGet.map((d) => (
-                      <tr
-                        key={d.ID_DOACOES}
-                        className="border-b border-[#97987E66] hover:bg-[#f9f9f9]"
-                      >
-                        <td className="px-4 py-2 bg-[#97987E66] text-[#505050] rounded-l-[15px]">
-                          {{
-                            1: "Plástico",
-                            2: "Alumínio",
-                            3: "Vidro",
-                            4: "Cobre",
-                            5: "Dispositivos Eletrônicos",
-                          }[d.ID_MATERIAIS] || d.ID_MATERIAIS}
-                        </td>
-                        <td className="px-4 py-2 bg-[#97987E66] text-[#505050]">
-                          {d.NR_QUANTIDADE} kg
-                        </td>
-                        <td className="px-4 py-2 bg-[#97987E66] text-[#505050]">
-                          {d.NR_DESCONTOS}%
-                        </td>
-                        <td className="px-4 py-2 bg-[#97987E66] text-[#505050] rounded-r-[15px]">
-                          {d.DT_DOACAO}
+              <div className="overflow-x-auto">
+                <table className="w-full table-auto border-collapse border border-[#97987E66]">
+                  <thead>
+                    <tr className="bg-[#f4f4f4]">
+                      <th className="px-4 py-2 text-[#505050] text-center whitespace-nowrap">
+                        Material
+                      </th>
+                      <th className="px-4 py-2 text-[#505050] text-center whitespace-nowrap">
+                        Quantidade (kg)
+                      </th>
+                      <th className="px-4 py-2 text-[#505050] text-center whitespace-nowrap">
+                        Descontos
+                      </th>
+                      <th className="px-4 py-2 text-[#505050] text-center whitespace-nowrap">
+                        Data
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {Array.isArray(DoacaoGet) && DoacaoGet.length > 0 ? (
+                      DoacaoGet.map((d) => (
+                        <tr
+                          key={d.ID_DOACOES}
+                          className="border-b border-[#97987E66] hover:bg-[#f9f9f9]"
+                        >
+                          <td className="px-4 py-2 bg-[#97987E66] text-[#505050] rounded-l-[15px]">
+                            {{
+                              1: "Plástico",
+                              2: "Alumínio",
+                              3: "Vidro",
+                              4: "Cobre",
+                              5: "Dispositivos Eletrônicos",
+                            }[d.ID_MATERIAIS] || d.ID_MATERIAIS}
+                          </td>
+                          <td className="px-4 py-2 bg-[#97987E66] text-[#505050]">
+                            {d.NR_QUANTIDADE} kg
+                          </td>
+                          <td className="px-4 py-2 bg-[#97987E66] text-[#505050]">
+                            {d.NR_DESCONTOS}%
+                          </td>
+                          <td className="px-4 py-2 bg-[#97987E66] text-[#505050] rounded-r-[15px]">
+                            {d.DT_DOACAO}
+                          </td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td
+                          colSpan={4}
+                          className="text-center text-[#505050] py-4"
+                        >
+                          Nenhuma doação registrada.
                         </td>
                       </tr>
-                    ))
-                  ) : (
-                    <tr>
-                      <td
-                        colSpan={4}
-                        className="text-center text-[#505050] py-4"
-                      >
-                        Nenhuma doação registrada.
-                      </td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
+                    )}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-    )
+        </main>
+      )
+    }
 }
--- gs-lal/src/app/placas_solares/page.tsx
+++ gs-lal/src/app/placas_solares/page.tsx
@@ -4,6 +4,7 @@ import { userAtom } from "@/atoms"
 import { useAtom } from "jotai"
 import { useEffect, useState } from "react"
 import { Doacao, Produtos } from "@/types"
+import { WarningOutlined } from "@ant-design/icons"
 
 export default function PlacasSolares() {
     const [user] = useAtom(userAtom)
@@ -13,99 +14,107 @@ export default function PlacasSolares() {
     const descontoTotal = doacao.reduce((total, d) => total + d.NR_DESCONTOS, 0)
 
     if (!user) {
-      router.push("/login")
-    }
-
-    const chamadaApiDoacao = async () => {
-      const response = await fetch(
-        `http://localhost:8080/doacao/${user?.ID_CONDOMINIO}`
+      const volta = setTimeout(() => {
+        router.push("/login")
+      }, 3000)
+      return (
+        <>
+          <div className="min-h-screen flex justify-center items-center">
+            <h1 className="p-8 bg-[#97987eae] text-white rounded-lg shadow-md text-center h-full text-3xl">
+              <WarningOutlined />  é necessario o login primeiro! <br />
+              Redirecionando para a página de login...
+            </h1>
+          </div>
+          {volta}
+        </>
       )
-      const data = await response.json()
-      setDoacao(data)
-    }
+    } else {
+      const chamadaApiDoacao = async () => {
+        const response = await fetch(
+          `http://localhost:8080/doacao/${user?.ID_CONDOMINIO}`
+        )
+        const data = await response.json()
+        setDoacao(data)
+      }
 
-    const chamadaApiProdutos = async () => {
-      const response = await fetch(
-        `http://localhost:8080/produtos`
-      )
-      const data = await response.json()
-      setProdutos(data)
-    }
+      const chamadaApiProdutos = async () => {
+        const response = await fetch(`http://localhost:8080/produtos`)
+        const data = await response.json()
+        setProdutos(data)
+      }
 
-    useEffect(() => {
-      chamadaApiDoacao()
-      chamadaApiProdutos()
-    }, [])
+      useEffect(() => {
+        chamadaApiDoacao()
+        chamadaApiProdutos()
+      }, [])
 
-    return (
-      <main className="flex flex-col items-center justify-center min-h-screen bg-[#f4f4f4] py-8 px-4">
-        {/* Exibição do desconto total */}
-        <div className="text-2xl font-semibold text-[#505050] mb-6">
-          Total de Descontos Acumulados: {descontoTotal}%
-        </div>
+      return (
+        <main className="flex flex-col items-center justify-center min-h-screen bg-[#f4f4f4] py-8 px-4">
+          {/* Exibição do desconto total */}
+          <div className="text-2xl font-semibold text-[#505050] mb-6">
+            Total de Descontos Acumulados: {descontoTotal}%
+          </div>
 
-        {/* Exibição de produtos */}
-        <section className="w-full max-w-4xl bg-white rounded-lg shadow-md p-6">
-          <h2 className="text-xl font-bold text-[#505050] mb-4">
-            Lista de Produtos
-          </h2>
-          {Array.isArray(produtos) && produtos.length > 0 ? (
-            produtos.map((produto) => {
+          {/* Exibição de produtos */}
+          <section className="w-full max-w-4xl bg-white rounded-lg shadow-md p-6">
+            <h2 className="text-xl font-bold text-[#505050] mb-4">
+              Lista de Produtos
+            </h2>
+            {Array.isArray(produtos) && produtos.length > 0 ? (
+              produtos.map((produto) => {
+                const semEstoque = produto.NR_QUANTIDADE <= 0
+                const comDesconto = descontoTotal > 0
+                const precoComDesconto = comDesconto
+                  ? (
+                      produto.NR_PRECO -
+                      (produto.NR_PRECO * descontoTotal) / 100
+                    ).toFixed(2)
+                  : null
 
-              const semEstoque = produto.NR_QUANTIDADE <= 0
-              const comDesconto = descontoTotal > 0
-              const precoComDesconto = comDesconto
-                ? (
-                    produto.NR_PRECO -
-                    (produto.NR_PRECO * descontoTotal) / 100
-                  ).toFixed(2)
-                : null
-
-              return (
-                <div
-                  key={produto.ID_PRODUTOS}
-                  className={`p-4 mb-4 rounded-lg shadow-md ${
-                    semEstoque
-                      ? "bg-gray-300 text-black"
-                      : "bg-white text-gray-800"
-                  }`}
-                >
-                  <h3 className="font-semibold text-lg">
-                    {produto.NM_PRODUTO}
-                  </h3>
-                  <p className="text-sm">
-                    {produto.DS_PRODUTO}
-                  </p>
-                  <p className="text-sm">
-                    Estoque:{" "}
-                    {semEstoque ? (
-                      <span className="text-red-500 font-bold">
-                        Sem estoque
-                      </span>
-                    ) : (
-                      produto.NR_QUANTIDADE
-                    )}
-                  </p>
-                  <p className="text-sm">
-                    Preço:{" "}
-                    {comDesconto ? (
-                      <span className="text-green-500">
-                        <span className="line-through text-red-500 mr-2">
-                          R$ {produto.NR_PRECO.toFixed(2)}
+                return (
+                  <div
+                    key={produto.ID_PRODUTOS}
+                    className={`p-4 mb-4 rounded-lg shadow-md ${
+                      semEstoque
+                        ? "bg-gray-300 text-black"
+                        : "bg-white text-gray-800"
+                    }`}
+                  >
+                    <h3 className="font-semibold text-lg">
+                      {produto.NM_PRODUTO}
+                    </h3>
+                    <p className="text-sm">{produto.DS_PRODUTO}</p>
+                    <p className="text-sm">
+                      Estoque:{" "}
+                      {semEstoque ? (
+                        <span className="text-red-500 font-bold">
+                          Sem estoque
                         </span>
-                        R$ {precoComDesconto}
-                      </span>
-                    ) : (
-                      `R$ ${produto.NR_PRECO.toFixed(2)}`
-                    )}
-                  </p>
-                </div>
-              )
-            })
-          ) : (
-            <p className="text-[#707070]">Nenhum produto encontrado.</p>
-          )}
-        </section>
-      </main>
-    )
+                      ) : (
+                        produto.NR_QUANTIDADE
+                      )}
+                    </p>
+                    <p className="text-sm">
+                      Preço:{" "}
+                      {comDesconto ? (
+                        <span className="text-green-500">
+                          <span className="line-through text-red-500 mr-2">
+                            R$ {produto.NR_PRECO.toFixed(2)}
+                          </span>
+                          R$ {precoComDesconto}
+                        </span>
+                      ) : (
+                        `R$ ${produto.NR_PRECO.toFixed(2)}`
+                      )}
+                    </p>
+                  </div>
+                )
+              })
+            ) : (
+              <p className="text-[#707070]">Nenhum produto encontrado.</p>
+            )}
+          </section>
+        </main>
+      )
+    }
 }
