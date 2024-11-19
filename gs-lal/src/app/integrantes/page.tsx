17e1152380c87a9c1de8a202efc5da575b1b547b
ludanii <ludanielle2005@hotmail.com>
2024-11-19 10:54:00 -0300

    Adicionando os links do GitHub e Linkedin dos integrantes.

--- gs-lal/src/app/integrantes/page.tsx
+++ gs-lal/src/app/integrantes/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import michelle from '@/public/img/michelle.jpeg';
 import luisa from '@/public/img/luisa.jpeg';
 import bruno from '@/public/img/bruno.png';
-import { GithubOutlined } from '@ant-design/icons';
+import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
 
 export default function Integrantes() {
     return (
@@ -14,48 +14,107 @@ export default function Integrantes() {
         </div>
 
         {/* Contêiner principal para os integrantes */}
-        <div className="flex justify-center gap-8 flex-wrap">
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 justify-items-center md:h-[120vh] lg:h-[50vh] h-[200vh] lg:gap-0 gap-4">
           {/* Michelle */}
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center bg-white rounded-xl p-7 justify-between shadow-md hover:shadow-2xl transition-shadow duration-300">
+            <h1 className="text-[#97987E] font-semibold text-2xl">
+              Michelle Pontenza
+            </h1>
             <div className="w-[208px] h-[208px] bg-[#97987E] rounded-[200px] flex items-center justify-center">
               <Image
                 src={michelle}
                 alt="Michelle"
-                className="rounded-full w-[120px] h-[120px] object-cover mt-20"
+                className="rounded-full w-[170px] h-[170px] object-cover mt-10"
               />
             </div>
-            <a href="#" className="text-[#505050] font-medium mt-4">
-              <GithubOutlined /> GitHub
-            </a>
+            <nav>
+              <ul className="grid grid-cols-2 text-center gap-2">
+                <li className="text-[#97987E] font-medium">557702</li>
+                <li className="text-[#97987E] font-medium">1TDSPG</li>
+                <li className="text-[#97987E] font-medium">
+                  <a
+                    href="https://github.com/michellepotenza01"
+                    target="_blank"
+                  >
+                    <GithubOutlined /> GitHub
+                  </a>
+                </li>
+                <li className="text-[#97987E] font-medium">
+                  <a
+                    href="https://www.linkedin.com/in/michelle-potenza-338510231/"
+                    target="_blank"
+                  >
+                    <LinkedinOutlined /> Linkedin
+                  </a>
+                </li>
+              </ul>
+            </nav>
           </div>
 
           {/* Luisa */}
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center bg-white rounded-xl p-7 justify-between shadow-md hover:shadow-2xl transition-shadow duration-300">
+            <h1 className="text-[#97987E] font-semibold text-2xl">
+              Luisa Danielle
+            </h1>
             <div className="w-[208px] h-[208px] bg-[#97987E] rounded-[200px] flex items-center justify-center">
               <Image
                 src={luisa}
                 alt="Luisa"
-                className="rounded-full w-[120px] h-[120px] object-cover mt-20"
+                className="rounded-full w-[170px] h-[170px] object-cover mt-10"
               />
             </div>
-            <a href="#" className="text-[#505050] font-medium mt-4">
-              <GithubOutlined /> GitHub
-            </a>
+            <nav>
+              <ul className="grid grid-cols-2 text-center gap-2">
+                <li className="text-[#97987E] font-medium">555292</li>
+                <li className="text-[#97987E] font-medium">1TDSPG</li>
+                <li className="text-[#97987E] font-medium">
+                  <a href="https://github.com/ludanii" target="_blank">
+                    <GithubOutlined /> GitHub
+                  </a>
+                </li>
+                <li className="text-[#97987E] font-medium">
+                  <a
+                    href="https://www.linkedin.com/in/luisa-danielle-663126274/"
+                    target="_blank"
+                  >
+                    <LinkedinOutlined /> Linkedin
+                  </a>
+                </li>
+              </ul>
+            </nav>
           </div>
 
           {/* Bruno Souza */}
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center bg-white rounded-xl p-7 justify-between shadow-md hover:shadow-2xl transition-shadow duration-300">
+            <h1 className="text-[#97987E] font-semibold text-2xl">
+              Bruno Souza
+            </h1>
             <div className="w-[208px] h-[208px] bg-[#97987E] rounded-[200px] flex items-center justify-center">
               <Image
                 src={bruno}
                 alt="Bruno Souza"
-                className="rounded-full w-[120px] h-[120px] object-cover mt-20"
+                className="rounded-full w-[170px] h-[170px] object-cover mt-10"
               />
             </div>
-
-            <a href="#" className="text-[#505050] font-medium mt-4">
-              <GithubOutlined /> GitHub
-            </a>
+            <nav>
+              <ul className="grid grid-cols-2 text-center gap-2">
+                <li className="text-[#97987E] font-medium">555292</li>
+                <li className="text-[#97987E] font-medium">1TDSPG</li>
+                <li className="text-[#97987E] font-medium">
+                  <a href="https://github.com/Brunosz2712" target="_blank">
+                    <GithubOutlined /> GitHub
+                  </a>
+                </li>
+                <li className="text-[#97987E] font-medium">
+                  <a
+                    href="https://www.linkedin.com/in/bruno-souza-79a354208/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+                    target="_blank"
+                  >
+                    <LinkedinOutlined /> Linkedin
+                  </a>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </main>
